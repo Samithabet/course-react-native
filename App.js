8ad26494292d7b3b6473d7f8a66e760de24f08de
Samithabet <sami.th71516@gmail.com>
2024-08-16 16:08:29 +0300

    refactoring:fix

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View,Button,TextInput } from "react-native";
 
 export default function App() {
+  const [person,Setperson]=useState({name:"Name",age:"Age"});
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+          <Text>Enter Name:</Text>
+          <TextInput
+          placeholder="Enter Name"
+          onChangeText={(value) => Setperson({ ...person, name: value })}
+          />
+          <Text>Enter Age:</Text>
+          <TextInput
+          placeholder="Enter Name"
+          onChangeText={(value) => Setperson({ ...person, age: value })}
+          />
+          <Text>Name:{person.name} Age:{person.age}</Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    textAlign: "center",
+    color: "blue",
+  },
+  header: {
+    backgroundColor: "red",
+    padding: 20,
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  body: {
+    backgroundColor: "blue",
+    padding: 20,
   },
 });
